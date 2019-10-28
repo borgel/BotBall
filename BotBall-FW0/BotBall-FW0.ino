64e44c8d183739b9455b54624f0c063e26219167
Kerry Scharfglass <iborgel@gmail.com>
2019-10-27 17:19:31 -0700

    Update pin assignments

--- BotBall-FW0/BotBall-FW0.ino
+++ BotBall-FW0/BotBall-FW0.ino
@@ -1,37 +1,52 @@
 // https://www.pololu.com/product/2135
 
+const int pwmAPin = 18;
+const int pwmBPin = 17;
+const int pwmMirror = 16;
 const int phaseAPin = 15;
-const int enableAPin = 16;
 const int phaseBPin = 18;
-const int enableBPin = 17;
+
+// single status LED
+const int ledPin = 19;
 
 void setup() {
+  // status LED
+  pinMode(ledPin, OUTPUT);
+  
   // setup PWM and direction pins
   pinMode(phaseAPin, OUTPUT);
   pinMode(phaseBPin, OUTPUT);
-  pinMode(enableAPin, OUTPUT);
-  pinMode(enableBPin, OUTPUT);
-  
-  // 8 bits
+  pinMode(pwmAPin, OUTPUT);
+  pinMode(pwmBPin, OUTPUT);
+
+  // active low
+  pinMode(pwmMirror, OUTPUT);
+  //analogWrite(pwmAPin, 100);
 }
 
 void loop() {
+  digitalWrite(ledPin, LOW);
+  
   // rotate in one way
   // dir
   digitalWrite(phaseAPin, HIGH);
   digitalWrite(phaseBPin, LOW);
 
   //PWM
-  analogWrite(enableAPin, 255);
-  analogWrite(enableBPin, 64);
+  // 8 bits
+  analogWrite(pwmAPin, 255);
+  analogWrite(pwmBPin, 64);
+  analogWrite(pwmAPin, 1);
   delay(1000);
+  digitalWrite(ledPin, HIGH);
 
   // rotate the other way
   digitalWrite(phaseAPin, LOW);
   digitalWrite(phaseBPin, HIGH);
   
-  analogWrite(enableAPin, 64);
-  analogWrite(enableBPin, 255);
+  analogWrite(pwmAPin, 64);
+  analogWrite(pwmBPin, 255);
+  analogWrite(pwmAPin, 250);
 
   delay(1000);
 }

b55e0e2b5357656f6e10c08a2e7a2a0514f0a01e
Kerry Scharfglass <iborgel@gmail.com>
2019-10-27 17:26:44 -0700

    Add RGB LED

--- BotBall-FW0/BotBall-FW0.ino
+++ BotBall-FW0/BotBall-FW0.ino
@@ -9,9 +9,18 @@ const int phaseBPin = 18;
 // single status LED
 const int ledPin = 19;
 
+// active low
+const int ledRPin = 6;
+const int ledGPin = 3;
+const int ledBPin = 4;
+
 void setup() {
   // status LED
   pinMode(ledPin, OUTPUT);
+  
+  pinMode(ledRPin, INPUT);
+  pinMode(ledGPin, INPUT);
+  pinMode(ledBPin, INPUT);
   
   // setup PWM and direction pins
   pinMode(phaseAPin, OUTPUT);
